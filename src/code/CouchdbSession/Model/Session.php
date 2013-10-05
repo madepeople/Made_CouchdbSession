17fea7b405ca36cee8bfbebe9275d81657ed9b33
Jonathan Selander <selander.jonathan@gmail.com>
2013-10-05 18:17:48 +0200

    first steps towards a couchdb session implementation

--- src/code/CouchdbSession/Model/Session.php
+++ src/code/CouchdbSession/Model/Session.php
@@ -1,40 +1,189 @@
 <?php
 /**
+ * CouchDB session read/write implementation. Uses document _ids
+ *
  * @author jonathan@madepeople.se
  */
 class Made_CouchdbSession_Model_Session
     implements Zend_Session_SaveHandler_Interface
 {
-    protected $_instance;
+    protected $_socket;
+    protected $_hostname = '127.0.0.1';
+    protected $_port = '5984';
+    protected $_username;
+    protected $_password;
+    protected $_databaseName = 'magento_session';
 
-    public function close()
+    const XML_BASE_PATH = 'global/couchdb_session';
+
+    /**
+     * Set up possible variable overrides from local XML definitions
+     */
+    public function __construct()
     {
+        $configKeys = array('hostname', 'port', 'username', 'password',
+            'databaseName');
 
+        foreach ($configKeys as $key) {
+            $xmlPath = self::XML_BASE_PATH . '/' . $key;
+            $value = trim((string)Mage::getConfig()->getNode($xmlPath));
+            if (!empty($value)) {
+                $member = "_" . $key;
+                $this->$member = $value;
+            }
+        }
+    }
+
+    public function __destruct()
+    {
+        session_write_close();
+    }
+
+    /**
+     * Setup save handler
+     *
+     * @return Made_CouchdbSession_Model_Session
+     */
+    public function setSaveHandler()
+    {
+        session_set_save_handler(
+            array($this, 'open'),
+            array($this, 'close'),
+            array($this, 'read'),
+            array($this, 'write'),
+            array($this, 'destroy'),
+            array($this, 'gc')
+        );
+
+        return $this;
+    }
+
+    /**
+     * Make a call to the CouchDB server and return the response
+     *
+     * @see http://wiki.apache.org/couchdb/Getting_started_with_PHP
+     * @param string $url  URL to change
+     * @param string $method  HTTP method used in this call
+     * @param array|string $data  Array or JSON encoded data to transmit
+     * @return array
+     * @throws Zend_Session_Exception
+     */
+    protected function _execute($url, $method = 'GET', $data = null)
+    {
+        $url = '/' . $this->_databaseName . $url;
+        $request = "$method $url HTTP/1.0\r\nHost: {$this->_host}\r\n";
+        if (!empty($this->_username) || !empty($this->_password)) {
+            $authentication = base64_encode($this->_username . ':' . $this->_password);
+            $request .= 'Authorization: Basic ' . $authentication . "\r\n";
+        }
+
+        if ($data) {
+            if (is_array($data)) {
+                // Assume the data is json encoded if it doesn't come as an array
+                $data = Mage::helper('core')->jsonEncode($data);
+            }
+            $request .= 'Content-Length: ' . strlen($data) . "\r\n";
+            $request .= 'Content-Type: application/json' . "\r\n\r\n";
+            $request .= $data . "\r\n";
+        } else {
+            $request .= "\r\n";
+        }
+
+        fwrite($this->_socket, $request);
+
+        $response = '';
+        while (!feof($this->_socket)) {
+            $response .= fgets($this->_socket);
+        }
+
+        list ($headers, $body) = explode("\r\n\r\n", $response);
+        return array(
+            'headers' => $headers,
+            'body_raw' => $body,
+            'body' => Mage::helper('core')->jsonDecode($body)
+        );
     }
 
     public function destroy($id)
     {
-
+        $data = $this->_execute('/' . $id, 'DELETE');
+        return true;
     }
 
     public function gc($maxlifetime)
     {
-
+        $documents = $this->_execute('/_all_docs');
+        foreach ($documents as $document) {
+            if (time()-$document['session_expiry'] > $maxlifetime) {
+                $this->destroy($document['_id']);
+            }
+        }
     }
 
+    /**
+     * Open a socket to the CouchDB server
+     *
+     * @param string $save_path
+     * @param string $name
+     * @return boolean
+     */
     public function open($save_path, $name)
     {
-        $this->_instance = new Couchbase(COUCHBASE_SERVER, COUCHBASE_USER,
-                COUCHBASE_PASS, COUCHBASE_BUCKET);
+        $this->_socket = @fsockopen($this->_host, $this->_port,
+                $errorCode = null, $errorString = null);
+
+        if (!$this->_socket) {
+            $message = 'Could not open CouchDB connection to ' . $this->_host
+                    . ':' . $this->_port . ' (' . $errorString . ')';
+
+            throw new Zend_Session_Exception($message, $errorCode);
+        }
     }
 
+    /**
+     * Close the (if open) socket connection to CouchDB
+     */
+    public function close()
+    {
+        fclose($this->_socket);
+        $this->_socket = null;
+    }
+
+    /**
+     * Retrieve the associated session document
+     *
+     * @param string $id
+     */
     public function read($id)
     {
-
+        $data = $this->_execute('/' . $id);
+        return $data['session_data'];
     }
 
+    /**
+     * Write update session data. If it fails due to an MVCC conflict, read
+     * the session again, and issue the write
+     *
+     * @param string $id
+     * @param array $data
+     */
     public function write($id, $data)
     {
+        $response = $this->_execute('/' . $id);
+        $data = $response['body'];
 
+        if (isset($data['error']) && $data['error'] === 'not_found') {
+            // We're writing a new session
+            $data['_id'] = $id;
+        }
+
+        $data['session_expiry'] = time()+Mage::getStoreConfig('web/cookie/cookie_lifetime');
+        $data['session_data'] = $data;
+
+        do {
+            $response = $this->_execute('/' . $id, 'PUT', $data);
+        } while (!(isset($response['body']['error']) && $response['body']['error'] === 'conflict'));
+
+        return true;
     }
 }
